8060c864519303d761308bbd4e7119053381ed35
Jean <jeancarlos.na@hotmail.com>
2023-11-21 23:21:31 -0300

    config base url

--- lib/repositories/connect_extends_repository.dart
+++ lib/repositories/connect_extends_repository.dart
@@ -8,21 +8,4 @@ class ConnectExtendsRepository {
   Environment environment = Environment();
     late String BASE_URL;
 
-    ConnectExtendsRepository() {
-    BASE_URL = environment.config.apiHost;
-  }
-
-  Future<void> onInit() async {
-    // Create an instance of the client
-    http.Client client = http.Client();
-
-    // Make a GET request to the BASE_URL
-    http.Response response = await client.get(Uri.parse(BASE_URL));
-
-    // Print the response
-    debugPrint(response.body);
-
-    // Close the client to free up resources
-    client.close();
-  }
 }
