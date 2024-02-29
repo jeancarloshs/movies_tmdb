ac4feeea408155eaf25cee4996bf937cb9f20c3a
Jean <jeancarlos.na@hotmail.com>
2024-02-28 23:02:09 -0300

    inserido pagina de video

---
+++ lib/pages/video_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class VideoPage extends StatefulWidget {
+  const VideoPage({required this.videoKey, super.key});
+  
+  final BuildContext videoKey;
+
+  @override
+  State<VideoPage> createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
